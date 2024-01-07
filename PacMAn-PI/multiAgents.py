e284c31faa18ec24780af32a7a3c1fcd52dd7798
Mohammad Hassan <heidary0081@gmail.com>
2024-01-07 21:43:36 +0330

    getAction Implemented

--- PacMAn-PI/multiAgents.py
+++ PacMAn-PI/multiAgents.py
@@ -1,106 +1,99 @@
-# multiAgents.py
-# --------------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-#
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+# PacMan AI agent using minimax with alpha beta prunning
+# Implemented by: Mohammad Hassan Heydari, Arian Jafari
 
 
-from util import manhattanDistance
 from game import Directions
-import random, util
-
+import random
+from util import manhattanDistance, lookup
 from game import Agent
 from pacman import GameState
 
 
 def scoreEvaluationFunction(currentGameState: GameState):
-    """
-    This default evaluation function just returns the score of the state.
-    The score is the same one displayed in the Pacman GUI.
-
-    This evaluation function is meant for use with adversarial search agents
-    """
     return currentGameState.getScore()
 
 
 class MultiAgentSearchAgent(Agent):
     def __init__(self, evalFn="scoreEvaluationFunction", depth="2", time_limit="6"):
         self.index = 0  # Pacman is always agent index 0
-        self.evaluationFunction = util.lookup(evalFn, globals())
+        self.evaluationFunction = lookup(evalFn, globals())
         self.depth = int(depth)
         self.time_limit = int(time_limit)
 
+    # evaluation function for minimax with alpha beta prunning
+    # heuristic is based on the distance of the closest food, the distance of the closest ghost and the number of ghosts in 1 distance
+    def evaluationFunction(self, game_state, action):
+        # get the next game state and the position of pacman
+        s_game_state = game_state.generatePacmanSuccessor(action)
+
+        return s_game_state.getScore()
 
 class AIAgent(MultiAgentSearchAgent):
-    def getAction(self, gameState: GameState):
-        """
-        Here are some method calls that might be useful when implementing minimax.
 
-        gameState.getLegalActions(agentIndex):
-        Returns a list of legal actions for an agent
-        agentIndex=0 means Pacman, ghosts are >= 1
+    # get action function for minimax with alpha beta prunning
+    def getAction(self, gameState):
 
-        gameState.generateSuccessor(agentIndex, action):
-        Returns the successor game state after an agent takes an action
+        def max_level(agent, depth, game_state, a, b):  # max_level function
+            # if the game is won/lost or the defined depth is reached return the utility
 
-        gameState.getNumAgents():
-        Returns the total number of agents in the game
+            value = -10e12 # set the value to a very small number
+            for s in game_state.getLegalActions(agent): # for each action calculate the value
 
-        gameState.isWin():
-        Returns whether or not the game state is a winning state
+                prunned = a_b_prunning(1, depth, game_state.generateSuccessor(agent, s), a, b) # calculate the value of the next level
 
-        gameState.isLose():
-        Returns whether or not the game state is a losing state
-        """
+                value = max(value, prunned ) # update the value
 
-        # TODO: Your code goes here
-        numberOfGhosts = gameState.getNumAgents() - 1
+                if value > b: # if the value is bigger than beta return the value
+                    return value
 
-        # Used only for pacman agent hence agentindex is always 0.
-        def maxLevel(gameState, depth):
-            currDepth = depth + 1
-            if gameState.isWin() or gameState.isLose() or currDepth == self.depth:  # Terminal Test
-                return self.evaluationFunction(gameState)
-            maxvalue = -999999
-            actions = gameState.getLegalActions(0)
-            for action in actions:
-                successor = gameState.generateSuccessor(0, action)
-                maxvalue = max(maxvalue, minLevel(successor, currDepth, 1))
-            return maxvalue
+                a = max(a, value) # update alpha
 
-        # For all ghosts.
-        def minLevel(gameState, depth, agentIndex):
-            minvalue = 999999
-            if gameState.isWin() or gameState.isLose():  # Terminal Test
-                return self.evaluationFunction(gameState)
-            actions = gameState.getLegalActions(agentIndex)
-            for action in actions:
-                successor = gameState.generateSuccessor(agentIndex, action)
-                if agentIndex == (gameState.getNumAgents() - 1):
-                    minvalue = min(minvalue, maxLevel(successor, depth))
-                else:
-                    minvalue = min(minvalue, minLevel(successor, depth, agentIndex + 1))
-            return minvalue
+            return value
 
-        # Root level action.
-        actions = gameState.getLegalActions(0)
-        currentScore = -999999
-        returnAction = ''
-        for action in actions:
-            nextState = gameState.generateSuccessor(0, action)
-            # Next level is a min level. Hence calling min for successors of the root.
-            score = minLevel(nextState, 0, 1)
-            # Choosing the action which is Maximum of the successors.
-            if score > currentScore:
-                returnAction = action
-                currentScore = score
-        return returnAction
+        def min_level(agent, depth, game_state, alpha, beta):  # min_level function
+            # if the game is won/lost or the defined depth is reached return the utility
 
-        # util.raiseNotDefined()
+            value = 10e12 # set the value to a very big number
+
+            next_agent = agent + 1  # calculate the next agent and increase depth accordingly.
+            if game_state.getNumAgents() == next_agent: # if the next agent is the last one
+                next_agent = 0 # set the next agent to pacman
+            if next_agent == 0: # if the next agent is pacman
+                depth += 1 # increase the depth
+
+            for s in game_state.getLegalActions(agent): # for each action calculate the value
+                value = min(value, a_b_prunning(next_agent, depth, game_state.generateSuccessor(agent, s), alpha, beta)) # calculate the value of the next level
+                if value < alpha: # if the value is smaller than alpha return the value
+                    return value
+                beta = min(beta, value) # update beta
+            return value
+
+        def a_b_prunning(agent, depth, game_state, alpha, beta): # alpha beta prunning function
+            if game_state.isLose() or game_state.isWin() or depth == self.depth:  # return the utility in case the defined depth is reached or the game is won/lost.
+                return self.evaluationFunction(game_state)
+
+            if agent == 0: # if the agent is pacman
+                return max_level(agent, depth, game_state, alpha, beta) # return the max level
+
+            else: # if the agent is a ghost
+                return min_level(agent, depth, game_state, alpha, beta) # return the min level
+
+
+        utility = -10e12 # set the utility to a very small number
+        action = Directions.NORTH # set the action to north
+        alpha = -10e12 # set alpha to a very small number
+        beta = 10e12 # set beta to a very big number
+
+        legal_actions = gameState.getLegalActions(0) # get the legal actions of pacman
+        random.shuffle(legal_actions)  # Shuffle the actions to add randomness
+
+        for s in legal_actions: # for each action calculate the utility
+            ghostValue = a_b_prunning(agent = 1,depth =  0, game_state= gameState.generateSuccessor(0, s), alpha = alpha, beta = beta) # calculate the utility of the next level
+            if ghostValue > utility: # if the utility is bigger than the previous one update the utility and the action
+                utility = ghostValue # update the utility
+                action = s # update the action
+            if utility > beta: # if the utility is bigger than beta return the utility
+                return utility
+            alpha = max(alpha, utility) # update alpha
+
+        return action
